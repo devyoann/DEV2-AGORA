5471cdde0329615a82134e82d7c58149f8a6ddbe
Yoann Di Maria <dm.yoann@gmail.com>
2018-01-29 11:30:15 +0100

    hello

--- index.php
+++ index.php
@@ -1,18 +1,21 @@
 <?PHP
 	require('global.php');
 
-	$thispage = array(
-		'id'		=> 1,
-		'name'		=> 'index',
-		'title'		=> 'mon index'
-	);
+	class thisPage {
+		public $id		= 1;
+		public $name	= 'index';
+		public $title	= 'TITLE DE TA PAGE';
+	}
+
+	$thisPage = new thisPage();
 ?><!DOCTYPE html>
 <html lang="fr">
 <head>
 	<meta charset="UTF-8">
-	<title><?PHP if(isset($thispage)) echo $thispage['title']; else echo 'Hello'; ?></title>
+	<title><?PHP if(isset($thisPage->title)) echo $thisPage->title; else echo $general->titledefault; ?></title>
 </head>
 <body>
 	<h1>Salut</h1>
+
 </body>
 </html>
