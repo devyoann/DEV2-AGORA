53bbe4b9292c2478d9ccb382652d1d6c2fbf7fbc
Alex <alexandreb@blacksmith.studio>
2018-01-29 11:45:58 +0100

    masonry

--- views/_layout/head.php
+++ views/_layout/head.php
@@ -3,5 +3,6 @@
 <head>
 <title>Title of the document</title>
 <link rel="stylesheet" href="../public/assets/css/app.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 <body>
--- views/feed.php
+++ views/feed.php
@@ -2,10 +2,17 @@
 <?php include '_layout/header.php' ?>
 <?php include '_layout/menu.php' ?>
 <section id="feed">
-    <div class="container">
+    <div class="container grid">
         <div class="post">
-            
+
         </div>
     </div>
 </section>
+<script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
+<script type="text/javascript">
+    $('.grid').masonry({
+    itemSelector: '.grid-item',
+    columnWidth: 200
+    });
+</script>
 <?php include '_layout/footer.php' ?>
