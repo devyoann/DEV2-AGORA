3923ce9f8169adbb50e64a5b363fb8695e90e0a9
Alex <alexandreb@blacksmith.studio>
2018-01-29 11:28:29 +0100

    menu

--- views/feed.php
+++ views/feed.php
@@ -1,3 +1,7 @@
 <?php include 'head.php' ?>
 <?php include 'header.php' ?>
+<?php include 'menu.php' ?>
+<section id="feed">
+
+</section>
 <?php include 'footer.php' ?>
--- views/head.php
+++ views/head.php
@@ -2,5 +2,6 @@
 <html>
 <head>
 <title>Title of the document</title>
+<link rel="stylesheet" href="../public/assets/css/app.css">
 </head>
 <body>
