14907ca0cb084ac3b3fda21940406131562b003e
Alex <alexandreb@blacksmith.studio>
2018-01-29 11:55:49 +0100

    link js

--- views/_layout/head.php
+++ views/_layout/head.php
@@ -4,5 +4,7 @@
 <title>Title of the document</title>
 <link rel="stylesheet" href="../public/assets/css/app.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
+<script src="../public/assets/js/feed.js" charset="utf-8"></script>
 </head>
 <body>
--- views/feed.php
+++ views/feed.php
@@ -2,17 +2,16 @@
 <?php include '_layout/header.php' ?>
 <?php include '_layout/menu.php' ?>
 <section id="feed">
-    <div class="container grid">
-        <div class="post">
-
-        </div>
+    <div class="container grid" data-masonry='{ "itemSelector": ".grid-item", "columnWidth": 200 }'>
+        <div class="post grid-item"></div>
+        <div class="post grid-item"></div>
+        <div class="post grid-item"></div>
+        <div class="post grid-item"></div>
+        <div class="post grid-item"></div>
+        <div class="post grid-item"></div>
+        <div class="post grid-item"></div>
     </div>
 </section>
 <script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
-<script type="text/javascript">
-    $('.grid').masonry({
-    itemSelector: '.grid-item',
-    columnWidth: 200
-    });
-</script>
+
 <?php include '_layout/footer.php' ?>
