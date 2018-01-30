f366b8e354dc3e1862facfdb961ea307df6023c8
Yoann Di Maria <dm.yoann@gmail.com>
2018-01-30 10:18:17 +0100

    push

---
+++ server.js
@@ -0,0 +1,22 @@
+const express	= require('express');
+const mysql		= require('mysql');
+const bodyParser= require('body-parser');
+const twig		= require('twig');
+
+const server	= express();
+
+server.set('view engine', 'twig');
+
+server.use('/assets/css',	express.static(__dirname + '/public/assets/css'));
+server.use('/assets/js',	express.static(__dirname + '/public/assets/js'));
+server.use('/assets/img',	express.static(__dirname + '/public/assets/img'));
+server.use('/assets/fonts',	express.static(__dirname + '/public/assets/fonts'));
+
+
+server.get('/', function(req, res) {
+	res.render(__dirname+'/views/index');
+});
+
+server.listen(1337, function() {
+	console.log('server ready');
+});
--- views/feed.php
+++
@@ -1,7 +0,0 @@
-<?php include 'head.php' ?>
-<?php include 'header.php' ?>
-<?php include 'menu.php' ?>
-<section id="feed">
-
-</section>
-<?php include 'footer.php' ?>
