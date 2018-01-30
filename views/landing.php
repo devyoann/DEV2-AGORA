70e45aedb73715f99c6d33e715629803f0af5ce6
brieuc <brieucmailpro@gmail.com>
2018-01-30 10:17:35 +0100

    Modifs landing

---
+++ views/landing.php
@@ -0,0 +1,10 @@
+<?php include '_layout/head.php' ?>
+<?php include '_layout/header.php' ?>
+<?php include '_layout/menu.php' ?>
+<main class="landing">
+    <div class="background"></div>
+    <section>
+        <h1>Le nouveau réseau social innovant</h1>
+    </section>
+</main>
+<?php include '_layout/footer.php' ?>
