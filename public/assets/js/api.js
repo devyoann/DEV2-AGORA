74223c4b1c4bf9bba24017f868bec5016909f069
brieuc <brieucmailpro@gmail.com>
2018-03-15 13:42:35 +0100

    Modif api weather

--- public/assets/js/api.js
+++ public/assets/js/api.js
@@ -5,13 +5,12 @@ $(document).ready(function() {
 
     var input = document.querySelector('#searchweather');
 
+    var apikeyopenweather = "appid=b2459737dfd1c99d3526c231400b6b68";
 
-
-    // open weather
+    // open weather today
     var getWeatherLocation = function() {
         var value = input.value;
-        var apikey = "appid=b2459737dfd1c99d3526c231400b6b68";
-        var _u = 'http://api.openweathermap.org/data/2.5/weather?q=' + value + '&units=metric&' + apikey;
+        var _u = 'http://api.openweathermap.org/data/2.5/weather?q=' + value + '&units=metric&' + apikeyopenweather;
 
         $.ajax({
             type: 'GET',
@@ -20,12 +19,28 @@ $(document).ready(function() {
             success: function(data, status) {
                 document.querySelector('h1').innerHTML = value;
                 document.querySelector('#locationdesc p').innerHTML = value + " - Météo pour aujourd'hui : Maximales de " + data.main.temp_max + "°" + " et Minimales de " + data.main.temp_min + "°";
-                console.log(data)
-                console.log(status)
+                document.querySelector('#locationinfotoday .todayinfo').innerHTML = data.main.temp +  "° C";
             },
         })
     }
     getWeatherLocation();
+
+    var getWeatherLocationForecast = function() {
+        var value = input.value;
+        var _u = 'http://api.openweathermap.org/data/2.5/forecast/?q=' + value + '&units=metric&' + apikeyopenweather;
+
+        $.ajax({
+            type: 'GET',
+            url: _u,
+            dataType: 'JSON',
+            success: function(data) {
+                for(i = 0; i < data.list.length; i++){
+                    console.log(data.list[i].dt_txt)
+                }
+            }
+        })
+    }
+    getWeatherLocationForecast();
 
     // pixabay
     var getImage = function() {
@@ -43,9 +58,8 @@ $(document).ready(function() {
             success: function(data) {
                 for (i = 0; i < data.hits.length; i++) {
                     document.querySelector('#locationimg .bg-img').style.backgroundImage = 'url(' + data.hits[getRandom(0, data.hits.length)].webformatURL + ')';
-                    console.log(data.hits[0].webformatURL)
+                    //console.log(data.hits[0].webformatURL)
                 }
-                console.log(data)
             }
         })
     }
